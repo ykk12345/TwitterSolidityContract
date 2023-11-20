3aeb40f8f0084e0afeacce0b2c7ba2b38f6ed5cc
Yash kumar kaushik <108153942+ykk12345@users.noreply.github.com>
2023-11-20 13:24:11 +0530

    Added the events for the tweet creation , tweet likes and tweet unlike
    In the enhanced version of the Solidity smart contract for a Twitter-like platform, several notable changes have been introduced:
    
    Event Emitters:
    
    Two new events, TweetCreated and TweetLiked, have been added to provide a mechanism for external entities to listen and react to specific contract activities. The TweetCreated event is emitted when a new tweet is successfully created, capturing the tweet's unique identifier, author, content, and timestamp. The TweetLiked event is emitted when a tweet is liked, providing details such as the liker's address, the tweet's author, the tweet's identifier, and the updated like count.
    Tweet Unliking Event:
    
    A third event, TweetUnliked, has been introduced. This event is emitted when a user unlikes a tweet, including information such as the unliker's address, the tweet's author, the tweet's identifier, and the updated like count after the unlike operation.
    Event Emission in Functions:
    
    The createTweet, likeTweet, and unlikeTweet functions have been updated to emit the corresponding events after successfully performing their respective actions. This allows external applications or scripts to listen for these events and respond accordingly.

--- twitter_basic_smart_contract.sol
+++ twitter_basic_smart_contract.sol
@@ -16,6 +16,10 @@ contract Twitter
 uint16 public MAX_TWEET_LENGTH = 280;
 address public owner;
 
+event TweetCreated(uint256 indexed id,address author, string content,uint256 timestamp);
+   event TweetLiked(address liker, address tweetAuthor, uint256 tweetId,uint256 newLikeCount);
+   event TweetUnliked(address unliker,address tweetAuthor,uint256 tweetId,uint256 newUnlikeCount);
+
    constructor(){
     owner = msg.sender;
    }
@@ -40,16 +44,23 @@ function changeTweetLength(uint16 newTweetLength) public onlyOwner{
       likes : 0
     });
     Tweets[msg.sender].push(newTweet);// saving the tweets in the mapping
+   // emiting the event
+    emit TweetCreated(newTweet.id,newTweet.author,newTweet.content,newTweet.timestamp);
   }
  function likeTweet(address author, uint256 id) external{
     require(Tweets[author][id].id==id,"Tweet does not exist");
     // above require its to make sure that you like the correct tweet
     Tweets[author][id].likes++;
+ // emiting the event
+    emit TweetLiked(msg.sender,author,id,Tweets[author][id].likes);
   }
 function unlikeTweet(address author, uint256 id) external{
     require(Tweets[author][id].id==id,"Tweet does not exist");
     require(Tweets[author][id].likes>0,"Tweet has no likes");
     Tweets[author][id].likes--;
+
+    // emiting the event
+    emit TweetUnliked(msg.sender,author,id,Tweets[author][id].likes);
   }
   function getTweet(uint _i) public view returns(Tweet memory){
     return Tweets[msg.sender][_i];
